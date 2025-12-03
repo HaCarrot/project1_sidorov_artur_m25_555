830e0472a17020f3cec3206e4379919a95abfbb3
Artur Sidorov <carrotorangelike@gmail.com>
2025-12-03 12:31:12 +0300

    Добавлены константы цвета

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,10 +1,10 @@
-from labyrinth_game.constants import ROOMS
+from labyrinth_game.constants import ROOMS, COLORS
 from labyrinth_game.utils import describe_current_room
 
 
 def show_inventory(game_state: dict):
     if len(game_state['player_inventory']) > 0:
-        print("\033[32m * ИНВЕНТАРЬ * \033[0m")
+        print(f"{COLORS['GREEN']} * ИНВЕНТАРЬ * {COLORS['RESET']}")
         for item in game_state['player_inventory']:
             print(f"\033[33m{item}\033[0m")
     else:
@@ -30,7 +30,7 @@ def move_player(game_state: dict, direction: str):
         game_state['steps_taken'] += 1
         describe_current_room(game_state)
     else:
-        print("\033[31mНельзя пойти в этом направлении!\033[0m")
+        print(f"{COLORS['RED']}Нельзя пойти в этом направлении!{COLORS['RESET']}")
 
 def take_item(game_state: dict, item_name: str):
     if item_name == 'treasure_chest':
@@ -39,13 +39,13 @@ def take_item(game_state: dict, item_name: str):
     and ROOMS[game_state['current_room']]['puzzle'] is None:
         game_state['player_inventory'].append(item_name) 
         ROOMS[game_state['current_room']]['items'].remove(item_name)
-        print(f"Вы подобрали: \033[35m{item_name}\033[0m")
+        print(f"Вы подобрали: {COLORS['MAGENTA']}{item_name}{COLORS['RESET']}")
         describe_current_room(game_state)
     elif item_name in ROOMS[game_state['current_room']]['items']\
     and ROOMS[game_state['current_room']]['puzzle'] is not None:
         print("Предмет так просто не даётся! Решите загадку.")
     else:
-        print("\033[31mТакого предмета здесь нет.\033[0m")
+        print(f"{COLORS['RED']}Такого предмета здесь нет.{COLORS['RESET']}")
 
 def use_item(game_state: dict, item_name: str):
     if item_name in game_state['player_inventory']:
@@ -55,7 +55,7 @@ def use_item(game_state: dict, item_name: str):
             case 'sword':
                 print('Вы чувствуете себя увереннее!')
             case 'bronze_box':
-                print('Вы открыли шкатулку и обнаружили там \033[35mrusty_key\033[0m"!')
+                print(f'Вы открыли шкатулку и обнаружили там {COLORS['MAGENTA']}rusty_key{COLORS['RESET']}!')
                 game_state['player_inventory'].append('rusty_key')
             case _:
                 print("Крутая штука! Жаль, игрок не знает как это использовать.")
